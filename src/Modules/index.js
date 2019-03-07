92b1bd581bba824c5d351a480d6dff70a810ad2b
fanpingyang <devfpy@gmail.com>
2019-03-07 17:34:14 +0800

    完善Modal组件的功能

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -4,24 +4,41 @@ import './index.scss';
 import BaseComponent from './baseComponent';
 import TableComponent from './userTableComponent';
 import Modal from '../components/Modal';
+import { error } from 'util';
 
 
 export default class index extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
 
         this.state = {
-            modalVisible:false
+            
         }
     }
 
-    handleOpentModalButtonOnClick = ()=>{
-        this.setState({
-            modalVisible:true
+    handleOpentModalButtonOnClick = () => {
+        this.myModal.openModal();
+    }
+
+    handleModalOnClosed = ()=>{
+        console.log(".......... Modal Closed");
+    }
+
+    handleModalOnOk = ()=>{
+        return new Promise((resolve, reject)=>{
+            setTimeout(() => {
+                // let error = {
+                //     message:"很严重的错误！"
+                // }
+                // reject(error);
+                resolve("success");
+            }, 3000);
         });
     }
-    
+
+
+
     render() {
         return (
             <Tab>
@@ -38,7 +55,47 @@ export default class index extends Component {
                 <Tab.Item title="Help" key="3">
                     <div className="appmain_container">
                         <Button type="primary" onClick={this.handleOpentModalButtonOnClick}>打开Modal</Button>
-                        <Modal title="我是弹窗" visible={this.state.modalVisible} hasMask={true} />
+                        <Modal 
+                            ref={node => (this.myModal = node)} 
+                            title="我是弹窗" 
+                            hasMask={true}
+                            width="600px"
+                            height ="400PX"
+                            type=""
+                            onClosed = {this.handleModalOnClosed}
+                            onOk = {this.handleModalOnOk}>
+                            <div>
+                                <span>我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容我是弹窗的内容</span><br />
+                            </div>
+
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                            <span>我是弹窗的内容2</span><br />
+                            <span>我是弹窗的内容3</span><br />
+                            <span>我是弹窗的内容4</span><br />
+                        </Modal>
                     </div>
                 </Tab.Item>
             </Tab>
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -8,62 +8,143 @@ export default class index extends Component {
 
     static propTypes = {
 
-        visible: PropTypes.bool.isRequired,
+
         hasMask: PropTypes.bool,
         target: PropTypes.any,
-        title:PropTypes.string,
-        width:PropTypes.number,
-        height:PropTypes.number,
-
-        onRequestClose: PropTypes.func,
+        title: PropTypes.string,
+        width: PropTypes.string,
+        height: PropTypes.string,
+        type:PropTypes.string, //primary empty
+        cancelButtonText: PropTypes.string,
+        okButtonText: PropTypes.string,
         onOpened: PropTypes.func,
-        onClosed: PropTypes.func
+        onClosed: PropTypes.func,
+        onOk: PropTypes.func,//parent component return a promise, success后 关闭modal
     }
 
     static defaultProps = {
-        visible: false,
         hasMask: true,
         target: "root",
-        title:"",
-        width:400,
-        height:320
+        title: "",
+        width: "400",
+        height: "320",
+        type:"",
+        cancelButtonText: "取消",
+        okButtonText: "确定"
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        console.log("........ this.props ", this.props);
+        this.state = {
+            visible: false,
+            loading: false,
+            cancelButtonDisable: false,
+            okButtonDisable: false,
+            closeButtonDisable: false
+        }
+
+        console.log(this.props);
     }
 
-    handleOnRequestClose = () => {
-        console.log(".......... Modal handleOnRequestClose");
+    openModal = () => {
+        this.setState({
+            visible: true
+        });
+    }
+
+    closeModal = () => {
+        this.setState({
+            visible: false
+        }, () => {
+            if (this.props.onClosed) {
+                this.props.onClosed();
+            }
+        });
+    }
+
+    handleCloseButtonOnClick = () => {
+        if (!this.state.closeButtonDisable) {
+            this.closeModal();
+        }
+    }
+
+    handleOkButtonOnClick = () => {
+        if (this.props.onOk) {
+            this.setState({
+                loading: true,
+                cancelButtonDisable: true,
+                okButtonDisable: true,
+                closeButtonDisable: true
+            });
+            this.props.onOk().then((data) => {
+                this.setState({
+                    loading: false,
+                    cancelButtonDisable: false,
+                    okButtonDisable: false,
+                    closeButtonDisable: false
+                }, () => {
+                    this.handleCloseButtonOnClick();
+                });
+
+            }).catch((error) => {
+                console.log("......... 发生错误:", error.message);
+                this.setState({
+                    loading: false,
+                    cancelButtonDisable: false,
+                    okButtonDisable: false,
+                    closeButtonDisable: false
+                });
+            });
+        }
     }
 
     render() {
+
+        let modalWidth = "";
+        let modalHeight = "";
+        if(this.props.width.indexOf("%") >0 || this.props.width.indexOf("px") >0 || this.props.width.indexOf("PX") >0){
+            modalWidth = this.props.width;   
+        }
+        else{
+            modalWidth = this.props.width+"px";
+        }
+
+        if(this.props.height.indexOf("%") >0 || this.props.height.indexOf("px") >0 || this.props.height.indexOf("PX") >0){
+            modalHeight = this.props.height;   
+        }
+        else{
+            modalHeight = this.props.height+"px";
+        }
+
+
         return (
             <Overlay
-                visible={this.props.visible}
-                // safeNode={() => this.btn}
+                visible={this.state.visible}
                 align="cc cc"
                 hasMask={this.props.hasMask}
                 disableScroll={false}
                 target={this.props.target}
-                wrapperClassName = "nodiot-modal-mask"
-                onRequestClose={this.handleOnRequestClose}>
-                <div className="nodiot-modal">
+                cache={false}
+                wrapperClassName="nodiot-modal-mask"
+                style={{ width: modalWidth }}
+                animation={{ in: 'expandInDown', out: 'expandOutUp' }}>
+                <div className={this.props.type==="primary"?"nodiot-modal primary":"nodiot-modal"}>
                     <div className="nodiot-modal-content">
-                        <div type="secondary" className="nodiot-modal-close nodiot-modal-close-x">
-                            <Icon type="close" size="small"/>
+                        <div type="secondary" className="nodiot-modal-close nodiot-modal-close-x" onClick={this.handleCloseButtonOnClick}>
+                            <Icon type="close" size="small" style={{ color: this.state.closeButtonDisable ? "#C4C6CF" : (this.props.type==="primary"?"#ffffff":"rgba(0,0,0,.85)") }} />
                         </div>
-                        <div className="nodiot-modal-header">
+                        <div className="nodiot-modal-header" >
                             <div className="nodiot-modal-title">{this.props.title}</div>
                         </div>
-                        <div className="nodiot-modal-body" style={{width:this.props.width, height:this.props.height}}>
-                        <span>我是内容</span>
+                        <div className="nodiot-modal-body" style={{height: modalHeight }}>
+                            {
+                                this.props.children
+                            }
                         </div>
                         <div className="nodiot-modal-footer">
-                            <Button type="secondary">取消</Button>
-                            <Button type="primary">确定</Button>
+                            <Button type="secondary" size="large" disabled={this.state.cancelButtonDisable} onClick={this.handleCloseButtonOnClick}>{this.props.cancelButtonText}</Button>
+                            <Button type="primary" size="large" loading={this.state.loading} disabled={this.state.okButtonDisable} onClick={this.handleOkButtonOnClick}>{this.props.okButtonText}</Button>
                         </div>
                     </div>
                 </div>
